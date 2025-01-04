0938509f6246eb85840bdc4fa6e73ea1fad0ca24
Icke758 <ickenaol@gmail.com>
2025-01-03 21:15:04 -0300

    first commit

---
+++ src/views/BaseView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const types = ref(['Batalhão', 'Medicina', 'Desenvolvimento', 'Comunicação'])
+</script>
+
+<template>
+  <h1>Base</h1>
+  <ul class="list">
+    <li v-for="(type, index) in types" :key="index">{{ type }}</li>
+  </ul>
+</template>
+
+<style scoped>
+.list {
+  display: flex;
+  gap: 25%;
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
+</style>
